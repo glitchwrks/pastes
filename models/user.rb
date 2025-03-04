ec7d3120d5d6880c7e8146d510b6ca929ce583f9
Jonathan Chapman <glitch@glitchwrks.com>
2025-03-04 15:43:35 -0500

    Removing leftover :has_many from User model import

--- models/user.rb
+++ models/user.rb
@@ -3,7 +3,5 @@ require 'uuidtools'
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :resource_records
-
   validates :login, :presence => true, :uniqueness => { :case_sensitive => false }
 end
